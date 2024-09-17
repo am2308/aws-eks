c05f7c15bad080c0001a3aae2b745b87cace1fcd
am2308 <b110020510@gmail.com>
2024-09-17 17:48:05 +0100

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-multienv-test"
-    key            = "multienv-eks/terraform-test.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "multienv-eks-terraform-state-lock"
-    encrypt        = true
-  }
-}
