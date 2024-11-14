13fe8259f1446c3d8dca9267f63ed7f0f7fcef59
am2308 <b110020510@gmail.com>
2024-11-14 09:43:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
-module "vpc" {
-  source = "./modules/vpc"
+#module "vpc" {
+#  source = "./modules/vpc"
 
-  project               = var.project
-  env                   = var.env
-  private_subnet_1_cidr = var.private_subnet_1_cidr
-  private_subnet_2_cidr = var.private_subnet_2_cidr
-  public_subnet_1_cidr  = var.public_subnet_1_cidr
-  public_subnet_2_cidr  = var.public_subnet_2_cidr
-  vpc_cidr              = var.vpc_cidr
+#  project               = var.project
+#  env                   = var.env
+#  private_subnet_1_cidr = var.private_subnet_1_cidr
+#  private_subnet_2_cidr = var.private_subnet_2_cidr
+#  public_subnet_1_cidr  = var.public_subnet_1_cidr
+#  public_subnet_2_cidr  = var.public_subnet_2_cidr
+#  vpc_cidr              = var.vpc_cidr
 
   # needed for shared load balancer integration
-  eks_cluster_name = "${var.project}-${var.env}-eks-1"
-}
+#  eks_cluster_name = "${var.project}-${var.env}-eks-1"
+#}
 
-module "tls" {
-  source                  = "./modules/tls"
-  project                 = var.project
-  env                     = var.env
-  cert_alias_domain_names = var.cert_alias_domain_names
-  cert_wildcard_domain    = var.cert_wildcard_domain
-}
+#module "tls" {
+#  source                  = "./modules/tls"
+#  project                 = var.project
+#  env                     = var.env
+#  cert_alias_domain_names = var.cert_alias_domain_names
+#  cert_wildcard_domain    = var.cert_wildcard_domain
+#}
 
 module "eks" {
   source = "./modules/eks"
@@ -27,8 +27,8 @@ module "eks" {
   project    = var.project
   env        = var.env
   subnet_ids = [
-    module.vpc.private_subnet_1_id,
-    module.vpc.private_subnet_2_id
+    subnet-0ed63c4fe1d0e90bf,
+    subnet-065229372f5a26920
   ]
   node_groups_config           = var.node_groups_config
   key_pair_name                = var.key_pair_name
